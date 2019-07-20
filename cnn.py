121b4d8954d81d918b1c93d57a73683c5ac25715
Harshit Pandey <harshit.pandey1798@gmail.com>
2019-07-20 21:03:40 +0530

    Add files via upload

--- cnn.py
+++ cnn.py
@@ -1,15 +1,3 @@
-# Convolutional Neural Network
-
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# pip install tensorflow
-
-# Installing Keras
-# pip install --upgrade keras
-
-# Part 1 - Building the CNN
 
 # Importing the Keras libraries and packages
 from keras.models import Sequential
@@ -18,30 +6,25 @@ from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 
-# Initialising the CNN
 classifier = Sequential()
 
-# Step 1 - Convolution
 classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+#32 is the no of feature detectors and 3X3 dimension of feature detector
+#image considered as 3 layers of rgb
 
-# Step 2 - Pooling
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
-# Adding a second convolutional layer
+
 classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
-# Step 3 - Flattening
 classifier.add(Flatten())
 
-# Step 4 - Full connection
 classifier.add(Dense(units = 128, activation = 'relu'))
 classifier.add(Dense(units = 1, activation = 'sigmoid'))
 
-# Compiling the CNN
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-# Part 2 - Fitting the CNN to the images
 
 from keras.preprocessing.image import ImageDataGenerator
 
@@ -64,6 +47,6 @@ test_set = test_datagen.flow_from_directory('dataset/test_set',
 
 classifier.fit_generator(training_set,
                          steps_per_epoch = 8000,
-                         epochs = 25,
+                         epochs = 10,
                          validation_data = test_set,
                          validation_steps = 2000)
